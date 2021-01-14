7f035af7ddb335c86075fd08281a548ae540507a
californi <bentor.siqueira@gmail.com>
2021-01-13 23:34:28 -0300

    host name
    host name

--- DockerizingOurApplication/nginxLoadBalancer/app/app.py
+++ DockerizingOurApplication/nginxLoadBalancer/app/app.py
@@ -3,10 +3,13 @@ from fastapi import FastAPI
 import logging
 from pydantic import BaseModel
 import os
+import socket
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
+
+print(socket.gethostname())
 
 class Failure(BaseModel):
     name: str
@@ -17,7 +20,7 @@ class Failure(BaseModel):
 
 @app.get("/")
 def helloamdocs():
-    message = "Application: {0}".format(os.environ['APP'])
+    message = "Application: {0}, Host: {1}".format(os.environ['APP'], socket.gethostname())
     return message
 
 
