042434d6bec1fdcbc8e765cf494c8182fa6f2eda
californi <bentor.siqueira@gmail.com>
2021-01-18 12:13:17 -0300

    timer
    timer

--- DockerizingOurApplication/amdocs_failuremonitor/api/app.py
+++ DockerizingOurApplication/amdocs_failuremonitor/api/app.py
@@ -1,10 +1,11 @@
 import logging
 from pydantic import BaseModel
 import httpx
+import time
 
 # security and target
 url_host = 'http://amdocsfailuremanager:5002'
-#url_host = 'http://nginx-ingress.failure.svc'  # verificar depois o motivo do endereco nao funcionar nessa versao... talvez faltou algum compra de configuracao
+# url_host = 'http://nginx-ingress.failure.svc'  # verificar depois o motivo do endereco nao funcionar nessa versao... talvez faltou algum compra de configuracao
 headers = {'Content-Type': 'application/json',
            'Authorization': 'Bearer {}'.format('bWljcm9jb250cm9sbGVycw==')}
 
@@ -13,15 +14,17 @@ def main():
 
     # defining data object
     failure = {"name": "aqui name",
-                "type": "aqui name",
-                "message": "aqui name",
-                "dateevent": "aqui name"}
+               "type": "aqui name",
+               "message": "aqui name",
+               "dateevent": "aqui name"}
 
     response = httpx.post(f"{url_host}/insufficientcpu",
-                                      headers=headers,
-                                      json=failure)
+                          headers=headers,
+                          json=failure)
 
     logging.info(response)
+
+    time.sleep(5)
 
 
 if __name__ == '__main__':
